74309e82065bfedcabb3af5d62d2822f76132e4c
Ankit Mishra <ankit.mishra@sunstone.in>
2025-07-01 16:39:42 +0530

    Update reverse.js

--- reverse.js
+++ reverse.js
@@ -1,8 +1,9 @@
+// Question 7: Reverse a String
 function reverseString(str) {
   return str.split('').reverse().join('');
 }
 
-// Example usage
+// Example:
 const input = "hello";
 const output = reverseString(input);
 console.log(output);
