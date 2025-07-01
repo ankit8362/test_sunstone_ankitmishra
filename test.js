9ad72559bd006092f0dfd9efaa92afe4274faf97
Ankit Mishra <ankit.mishra@sunstone.in>
2025-07-01 16:42:38 +0530

    Delete test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-// Questions 8: Check if a String is a Palindrome
-function isPalindrome(str) {
-    const cleaned = str.replace(/\s+/g, '').toLowerCase();
-   
-    return cleaned === cleaned.split('').reverse().join('');
-}
-console.log(isPalindrome("MARKRAM"));
